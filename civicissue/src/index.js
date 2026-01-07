9d8a4c27392a5ef7c033d0ddb8e65b9b5333d17a
ARYAN BHOYA <aryanbhoya01@gmail.com>
2026-01-07 17:30:36 +0530

    Removed civicissue folder

--- civicissue/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App.js"; // fully-specified import to satisfy ESM resolution
-import "./index.css"; // optional - keep if you have global styles
-
-const container = document.getElementById("root");
-const root = createRoot(container);
-root.render(<App />);
