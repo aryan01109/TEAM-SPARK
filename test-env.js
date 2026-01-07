9e416211aca3e2c0e7ad12a9d5c98e436d5dfff1
ARYAN BHOYA <aryanbhoya01@gmail.com>
2026-01-07 08:51:31 +0530

    initial file add

---
+++ test-env.js
@@ -0,0 +1,12 @@
+﻿const assert = require('assert');
+try { require('dotenv').config(); } catch (e) { /* dotenv might not be installed */ }
+console.log('Node version:', process.version);
+console.log('Loaded dotenv:', !!process.env.DOTENV);
+console.log('TEST_ENV before:', process.env.TEST_ENV);
+if (process.env.TEST_ENV === 'expected_value') {
+  console.log('PASS: TEST_ENV matches expected_value');
+  process.exit(0);
+} else {
+  console.error('FAIL: TEST_ENV not set to expected_value');
+  process.exit(1);
+}
